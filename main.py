a1ec42685f24891dd4ddaa89a5880f3c8bd84d3a
bzq360 <2733938791@qq.com>
2020-07-21 17:51:41 +0100

    initial main

---
+++ main.py
@@ -0,0 +1,34 @@
+# number of maximum generations
+max_generations = 100
+
+# population size
+population_size = 20
+
+# fitness function types
+fitness_types = [
+    'original',
+    'decision',
+    'arjae',
+    'checkpoint'
+]
+
+# benchmark problems
+problems = [
+    'depth_first_search',
+    'detect_cycle',
+    'find_in_sorted',
+    'get_factors',
+    'hanoi',
+    'is_valid_parenthesization',
+    'knapsack',
+    'levenshtein',
+    'lis',
+    'mergesort',
+    'overflow',
+    'next_permutation',
+    'powerset',
+    'quicksort',
+    'rpn_eval',
+    'shortest_path_lengths'
+]
+
